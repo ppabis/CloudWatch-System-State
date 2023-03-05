753d5601bdac07467e3bcd75853ecbf47a363722
ppabis <piotr@pabis.eu>
2023-03-05 11:37:28 +0100

    Put metrics policy

--- role.tf
+++ role.tf
@@ -17,3 +17,23 @@ resource "aws_iam_instance_profile" "ec2-cloudwatch" {
   name = "ec2-cloudwatch"
   role = aws_iam_role.ec2-cloudwatch.name
 }
+
+resource "aws_iam_policy" "cloudwatch" {
+    name = "CloudWatch-PutMetric"
+    policy = <<-EOF
+    {
+      "Version": "2012-10-17",
+      "Statement": [ {
+        "Sid": "Stmt1",
+        "Effect": "Allow",
+        "Action": [ "cloudwatch:PutMetricData" ],
+        "Resource": "*"
+      } ]
+    }
+    EOF
+}
+
+resource "aws_iam_role_policy_attachment" "cloudwatch" {
+    role = aws_iam_role.ec2-cloudwatch.name
+    policy_arn = aws_iam_policy.cloudwatch.arn
+}
